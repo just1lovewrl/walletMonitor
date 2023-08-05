59c3b30441e3e7c90827496a01e4f504a1daaefe
just1lovewrl <90997099+just1lovewrl@users.noreply.github.com>
2023-08-05 22:46:37 +0800

    Update getEthPrice.js
    Update getEthPrice.js

--- src/utils/ether/getEthPrice.js
+++ src/utils/ether/getEthPrice.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 const getEthPrice = async () => {
     try {
-        const ethResponse = await axios.post('https://mainnet.era.zksync.io/', {
-            id: 42,
-            jsonrpc: '2.0',
-            method: 'zks_getTokenPrice',
-            params: ['0x0000000000000000000000000000000000000000'],
-        });
-        return ethResponse.data.result
+        const options = {
+            method: 'GET',
+            url: 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD',
+        }
+        let response = await axios.request(options)
+        return response.data['USD']
     } catch (e) {
         console.log(e)
-        return 0
+        return "--"
     }
+
 }
 
 export default getEthPrice
